84b75ad01dcf6379cb4d128cd10c42ab2a0a23ec
Enoc Dominguez <enocescazan@hotmail.com>
2024-08-15 11:24:22 -0600

    Add About section

---
+++ src/sections/About.tsx
@@ -0,0 +1,17 @@
+export default function About() {
+  return (
+    <section className="bg-white py-28">
+      <div className="container">
+        <h2 className="title">¿Quién soy yo?</h2>
+        <p className="mt-10 text-blue-900 mx-auto text-center font-medium">
+          Soy un entusiasta del desarrollo frontend, siempre buscando
+          aprendizaje para mejorar la experiencia de usuario mediante la
+          creación de interfaces elegantes y eficientes. Me considero una
+          persona curiosa, con un enfoque en aprender, y aplicarlo para resolver
+          problemas de manera creativa. Disfruto mucho de lo que hago, y me
+          apasiona trabajar en proyectos que combinan diseño y tecnología.
+        </p>
+      </div>
+    </section>
+  );
+}
