4a4786a190d95ce1258922e1046d102c675cafe0
Enoc Dominguez <enocescazan@hotmail.com>
2024-08-15 11:30:02 -0600

    Add Header, Hero, About And Technologies sections to App file

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import About from "./sections/About";
+import Header from "./sections/Header";
+import Hero from "./sections/Hero";
+import Technologies from "./sections/Technologies";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <About />
+      <Technologies />
+    </>
+  );
+}
+
+export default App;
