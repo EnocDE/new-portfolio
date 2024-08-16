bda9ce0ff7339a02ed57cfa3ce18a85cb6d34470
Enoc Dominguez <enocescazan@hotmail.com>
2024-08-16 12:24:09 -0600

    Add my profile picture to the about section

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -11,6 +11,15 @@ export default function About() {
           problemas de manera creativa. Disfruto mucho de lo que hago, y me
           apasiona trabajar en proyectos que combinan diseño y tecnología.
         </p>
+        <picture className="rounded-full overflow-hidden block h-72 w-72 md:h-96 md:w-96 transition mt-10 mx-auto bg-slate-100 border-2 border-slate-200/15">
+          <source srcSet="./src/assets/picture.webp" type="image/webp" />
+          <source srcSet="./src/assets/picture.png" type="image/png" />
+          <img
+            src="./src/assets/picture.png"
+            alt="Me"
+            className="-scale-x-100"
+          />
+        </picture>
       </div>
     </section>
   );
