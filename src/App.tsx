3e6c0b7cafe4a20bc0c8c41dc402f977d8bcebaf
Enoc Dominguez <enocescazan@hotmail.com>
2024-08-16 12:31:49 -0600

    Add Project section to App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import About from "./sections/About";
 import Header from "./sections/Header";
 import Hero from "./sections/Hero";
+import Projects from "./sections/Projects";
 import Technologies from "./sections/Technologies";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <About />
       <Technologies />
+      <Projects />
     </>
   );
 }
