ac3ff4dc47a74bd78928ff847412803707511022
Enoc Dominguez <enocescazan@hotmail.com>
2024-08-20 14:53:29 -0600

    Add zoom animation to project image on mouse hover

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -111,7 +111,7 @@ export default function Projects() {
                 }}
               >
                 <div className="overflow-hidden rounded-xl">
-                  <img src={`./src/assets/projects/${image}.png`} alt="" />
+                  <img src={`./src/assets/projects/${image}.png`} alt="" className="hover:scale-[1.15] transition" />
                 </div>
                 <h3 className="font-bold text-2xl text-blue-950 tracking-tight">
                   {name}
