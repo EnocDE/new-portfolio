bb9fc8615674d42b315d9b2df8a7728507d586ca
Enoc Dominguez <enocescazan@hotmail.com>
2024-08-19 21:00:51 -0600

    Add animations to title, description and images card on scroll

--- src/sections/Technologies.tsx
+++ src/sections/Technologies.tsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion";
+
 const TECH_DATA = [
   {
     name: "HTML",
@@ -45,22 +47,77 @@ export default function Technologies() {
   return (
     <section className="bg-white overflow-x-clip pb-28">
       <div className="container">
-        <h2 className="title">Tecnologías</h2>
+        <motion.h2
+          className="title"
+          initial={{
+            opacity: 0,
+            translateX: 100,
+          }}
+          whileInView={{
+            opacity: 1,
+            translateX: 0,
+          }}
+          viewport={{
+            once: true,
+          }}
+        >
+          Tecnologías
+        </motion.h2>
         <div className="relative">
-          <p className="mt-10 text-center text-blue-900 font-medium">
+          <motion.p
+            className="mt-10 text-center text-blue-900 font-medium"
+            initial={{
+              opacity: 0,
+              translateX: 100,
+            }}
+            whileInView={{
+              opacity: 1,
+              translateX: 0,
+            }}
+            viewport={{
+              once: true,
+            }}
+            transition={{
+              delay: 0.2,
+            }}
+          >
             Me encanta trabajar con tecnologías modernas que me permiten crear
             experiencias web atractivas y eficientes. Estas son algunas de las
             herramientas y lenguajes que utilizo:
-          </p>
+          </motion.p>
           <ul className="grid grid-cols-2 md:grid-cols-5 justify-items-center gap-5 mt-10">
-            {TECH_DATA.map(({ name, icon }) => (
-              <img
+            {TECH_DATA.map(({ name, icon }, index) => (
+              <motion.li
                 key={name}
-                src={`./src/assets/${icon}.svg`}
-                alt={`${name} Image`}
-                width={150}
-                className="drop-shadow-lg"
-              />
+                initial={{
+                  opacity: 0,
+                  translateX: 100,
+                }}
+                whileInView={{
+                  opacity: 1,
+                  translateX: 0,
+                }}
+                viewport={{
+                  once: true,
+                }}
+                transition={{
+                  delay: index * .2,
+                }}
+              >
+                <motion.img
+                  src={`./src/assets/${icon}.svg`}
+                  alt={`${name} Image`}
+                  width={150}
+                  className="drop-shadow-lg select-none"
+                  drag
+                  dragConstraints={{
+                    top: 0,
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                  }}
+                />
+              </motion.li>
             ))}
           </ul>
           <img
